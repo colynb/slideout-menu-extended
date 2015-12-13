637b72a846717147405f95b9dbdeeabee7ffead0
Colyn Brown <colyn.brown@gmail.com>
2015-12-13 11:38:55 -0700

    Added gulp-data, dotenv

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,22 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
+var data = require('gulp-data');
+var fs = require('fs');
+
+// load env vars from .env
+require('dotenv').load();
+
+function loadJson(file, cb) {
+  fs.readFile('./src/data.json', 'utf8', function(errRead, data) {
+    if (errRead) {
+      cb(undefined, {});
+    } else {
+      var jsData = JSON.parse(data);
+      cb(undefined, jsData);
+    }
+  });
+}
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'jade'], function() {
@@ -12,6 +28,7 @@ gulp.task('serve', ['sass', 'jade'], function() {
 
   gulp.watch("./src/sass/*.sass", ['sass']);
   gulp.watch("./src/**/*.jade", ['jade']);
+  gulp.watch("./src/**/*.json", ['jade']);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -23,9 +40,8 @@ gulp.task('sass', function() {
 });
 
 gulp.task('jade', function() {
-  var YOUR_LOCALS = {};
-
   gulp.src('./src/**/*.jade')
+    .pipe(data(loadJson))
     .pipe(jade())
     .pipe(gulp.dest('./public/'))
     .pipe(browserSync.stream());
